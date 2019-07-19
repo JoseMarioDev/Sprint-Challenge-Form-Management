00f0ac8d1b4bd6398f6e06bc4e7e74b9bed7b3a3
Jose Montero Jr <josemariodev@gmail.com>
2019-07-19 13:39:54 -0500

    setup links

--- form-app/src/App.js
+++ form-app/src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Form from './components/Form';
-import { Route } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
 import ProtectedRoute from './components/ProtectedRoute';
 import RecipesList from './components/RecipesList';
-
 
 function App() {
   return (
     <div className='App'>
+      <Link to='/'>Register </Link>
+      <Link to='/restricted/data'>Recipes</Link>
       <Route exact path='/' component={Form} />
       <ProtectedRoute exact path='/restricted/data' component={RecipesList} />
     </div>
